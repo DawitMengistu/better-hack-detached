8ce6aaeccd23c12beaa51bbe6a085bcca853552a
Naol Fekadu <nf.naol9@gmail.com>
2025-10-04 19:49:47 +0300

    backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import { Elysia } from 'elysia'
+import { node } from '@elysiajs/node'
+
+const app = new Elysia({ adapter: node() })
+	.get('/', () => '🦊 Hello Elysia')
+	.listen(3000, ({ hostname, port }) => {
+		console.log(
+			`🦊 Elysia is running at ${hostname}:${port}`
+		)
+	})
+
